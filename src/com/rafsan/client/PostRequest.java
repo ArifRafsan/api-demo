e17b2d4b62b5d8e812209713d55fb1aee00414c5
ArifRafsan <rafsan9994@gmail.com>
2020-06-30 06:25:56 +0600

    api-demo and http status code

---
+++ src/com/rafsan/client/PostRequest.java
@@ -0,0 +1,77 @@
+package com.rafsan.client;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Rafsan
+ */
+public class PostRequest {
+
+    public static void main(String[] args) {
+
+        String URL = "URL which one trying to consume";
+        String payLoad = "{\"key\": \"WriteSomething\",\"content\": \"describe it\"}";
+
+        try {
+
+            
+            JSONObject jsonObject = new JSONObject(payLoad);
+
+            URL url = new URL(URL);
+            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.setDoOutput(true);
+            httpConnection.setRequestMethod("POST");
+            httpConnection.setRequestProperty("Content-Type", "application/json");
+            httpConnection.setRequestProperty("Accept", "application/json");
+            // Not required
+            // urlConnection.setRequestProperty("Content-Length", String.valueOf(input.getBytes().length));
+
+            // Writes the JSON parsed as string to the connection
+            DataOutputStream wr = new DataOutputStream(httpConnection.getOutputStream());
+            wr.write(jsonObject.toString().getBytes());
+            Integer responseCode = httpConnection.getResponseCode();
+
+            BufferedReader bufferedReader;
+
+            // Creates a reader buffer
+            if (responseCode > 199 && responseCode < 300) {
+                bufferedReader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
+            } else {
+                bufferedReader = new BufferedReader(new InputStreamReader(httpConnection.getErrorStream()));
+            }
+
+            // To receive the response
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            bufferedReader.close();
+
+            // Prints the response
+            System.out.println(content.toString());
+
+        } catch (Exception e) {
+            System.out.println("Error Message");
+            System.out.println(e.getClass().getSimpleName());
+            System.out.println(e.getMessage());
+        }
+    }
+}
